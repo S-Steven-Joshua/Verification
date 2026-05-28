2af425031d5ff250a46b1c573b12411d4cbccac0
S Steven Joshua <136824140+S-Steven-Joshua@users.noreply.github.com>
2026-05-28 16:20:32 +0530

    Update fifo_assertion.sv

--- fifo_fwft/fifo_assertion.sv
+++ fifo_fwft/fifo_assertion.sv
@@ -16,7 +16,7 @@ endproperty:fwft
 
 property full_property;
     @(posedge vif.clk) disable iff(vif.rst)
-    vif.w_en && vif.full |=> $stable(vif.data_in);
+    vif.w_en && vif.full |=> $stable(vif.full);
 endproperty:full_property
 
 property safety;
